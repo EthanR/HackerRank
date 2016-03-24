98738b48116c29c25876f701f7bbf79743056caa
EthanR <rivettea@hotmail.com>
2016-03-24 18:22:26 -0400

    Initial Commit for the Solve Me First Problem
    Initial Commit for the Solve Me First Problem.

---
+++ Solve Me First/Source/SolveMeFirst/SolveMeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {        
+        static void Main(String[] args)
+        {
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            int sum = SolveMeFirst(firstValue, secondValue);
+            Console.WriteLine(sum);
+        }
+    
+        static int SolveMeFirst(int firstValue, int secondValue)
+        {
+            return firstValue + secondValue;
+        }
+    }
+}
