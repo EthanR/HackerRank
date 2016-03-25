e647fe3410e8d65527426f8ce843c7360e56260b
EthanR <rivettea@hotmail.com>
2016-03-25 02:23:20 -0400

    Initial commit for Simple Array Sum solution
    Initial commit for Simple Array Sum solution. See the Problem Statement
    for more details.

---
+++ Simple Array Sum/Source/SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var readLine = Console.ReadLine();
+            if (readLine != null)
+            {
+                string[] stringIntegers = readLine.Split(' ');
+                int[] integersToSum = Array.ConvertAll(stringIntegers, int.Parse);
+                Console.WriteLine(integersToSum.Sum());
+            }
+        }
+    }
+}
