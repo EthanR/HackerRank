f19dad1eb8bd5cf8c652a817cf55871010d74e7e
EthanR <rivettea@hotmail.com>
2016-03-25 01:43:27 -0400

    Initial commit for A Very Big Sum solution
    Initial commit for A Very Big Sum solution. Read the Problem Statement
    for more information.

---
+++ A Very Big Sum/Source/AVeryBigSum/AVeryBigSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            string readLine = Console.ReadLine();
+            if (readLine != null)
+            {
+                string[] integerStrings = readLine.Split(' ');
+                long [] integersToSum = Array.ConvertAll(integerStrings, long.Parse);
+                Console.WriteLine(integersToSum.Sum());
+            }
+        }
+    }
+}
