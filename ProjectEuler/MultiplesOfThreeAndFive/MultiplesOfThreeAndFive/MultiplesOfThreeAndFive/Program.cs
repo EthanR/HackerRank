cd790a20da533122277d2d3637148d1bde76f6d1
EthanR <rivettea@hotmail.com>
2016-03-26 20:01:39 -0400

    Commit code for Multiples of 3 and 5 solution
    Code commit for the solution to the 1st Project Euler problem, Multiples
    of 3 and 5.

---
+++ ProjectEuler/MultiplesOfThreeAndFive/MultiplesOfThreeAndFive/MultiplesOfThreeAndFive/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOfThreeAndFive
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            List<long> sums = new List<long>();
+            int numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numberOfTestCases; i++)
+            {
+                long testCase = Convert.ToInt64(Console.ReadLine());
+                sums.Add(GetSum(testCase));
+            }
+
+            foreach (long sum in sums)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+
+        static long GetSum(long upperBound)
+        {
+            long numberOfMultiplesOfThree = (upperBound - 1) / 3;
+            long numberOfMultiplesOfFive = (upperBound - 1) / 5;
+            long numberOfMultiplesOfFifteen = (upperBound - 1) / 15;
+
+            long multiplesOfThree = 3 * (numberOfMultiplesOfThree * (numberOfMultiplesOfThree + 1) / 2);
+            long multiplesOfFive = 5 * (numberOfMultiplesOfFive * (numberOfMultiplesOfFive + 1) / 2);
+            long multiplesOfFifteen = 15 * (numberOfMultiplesOfFifteen * (numberOfMultiplesOfFifteen + 1) / 2);
+
+            return multiplesOfThree + multiplesOfFive - multiplesOfFifteen;
+        }
+    }
+}
