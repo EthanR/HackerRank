6e3fca28364e667b4d5d9a5505103a828c2b8218
EthanR <rivettea@hotmail.com>
2016-04-02 23:25:46 -0400

    Initial commit for Basic Statistic Warm Up
    Initial commit for Basic Statistic Warm Up console application and
    corresponding unit test.

---
+++ Intro to Statistics/BasicStatisticsWarmUp/BasicStatisticsWarmUp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStatisticsWarmUp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfElements = int.Parse(Console.ReadLine());
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(number => number).ToList();
+
+            decimal mean = GetMean(numberOfElements, numbers);
+            decimal median = GetMedian(numberOfElements, numbers);
+            int mode = GetMode(numbers);
+            decimal standardDeviation = GetStandardDeviation(numberOfElements, numbers, mean);
+
+            Console.WriteLine("{0:F1}", mean);                       
+            Console.WriteLine("{0:F1}", median);
+            Console.WriteLine(mode);
+            Console.WriteLine("{0:F1}", standardDeviation);
+        }
+
+        public static decimal GetMean(int numberOfElements, List<int> numbers)
+        {
+            int sum = numbers.Sum();
+            return sum / (decimal)numberOfElements;
+        }
+
+        public static decimal GetMedian(int numberOfElements, List<int> numbers)
+        {
+            if (numberOfElements % 2 == 0)
+            {
+                return (numbers[(numberOfElements / 2) - 1] + numbers[(numberOfElements / 2)]) / 2M;
+            }
+
+            return decimal.Round(numbers[numberOfElements / 2], 1);
+        }
+
+        public static int GetMode(List<int> numbers)
+        {
+            IEnumerable<IGrouping<int, int>> groupings = numbers.GroupBy(number => number).OrderByDescending(group => group.Count());
+            IEnumerable<IGrouping<int, int>> potentialModes = groupings.Where(grouping => grouping.Count() == groupings.First().Count()).OrderBy(grouping => grouping.Key);
+            return potentialModes.First().Key;
+        }
+
+        public static decimal GetStandardDeviation(int numberOfElements, List<int> numbers, decimal mean)
+        {
+            decimal intermediarySum = numbers.Select(number => (number - mean) * (number - mean)).Sum();
+            return decimal.Round((decimal)Math.Sqrt((double)(intermediarySum / numberOfElements)), 1);
+        }
+    }
+}
---
+++ Intro to Statistics/BasicStatisticsWarmUp/BasicStatisticsWarmUpTests/ProgramTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BasicStatisticsWarmUp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicStatisticsWarmUpTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        //Sample Input
+
+        //10
+        //64630 11735 14216 99233 14470 4978 73429 38120 51135 67060
+
+        //Sample Output
+
+        //43900.6
+        //44627.5
+        //4978
+        //30466.9
+
+        [TestMethod]
+        public void TestCaseOne()
+        {
+            // Arrange
+            decimal expectedMean = 43900.6M;
+            decimal expectedMedian = 44627.5M;
+            int expectedMode = 4978;
+            decimal expectedStandardDeviation = 30466.9M;
+            List<int> numbers = new List<int> { 64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060 };
+                
+            // Act
+            decimal actualMean = Program.GetMean(10, numbers);
+            decimal actualMedian = Program.GetMedian(10, numbers.OrderBy(number => number).ToList());
+            decimal actualMode = Program.GetMode(numbers);
+            decimal actualStandardDeviation = Program.GetStandardDeviation(10, numbers, expectedMean);
+
+            // Assert
+            Assert.AreEqual(expectedMean, actualMean);
+            Assert.AreEqual(expectedMedian, actualMedian);
+            Assert.AreEqual(expectedMode, actualMode);
+            Assert.AreEqual(expectedStandardDeviation, actualStandardDeviation);
+        }
+    }
+}
