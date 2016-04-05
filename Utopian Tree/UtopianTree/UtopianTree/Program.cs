13452bcc0829e5ecfcfa364629d22358800bfa0f
EthanR <rivettea@hotmail.com>
2016-04-05 17:18:42 -0400

    Solution and Problem Statement for Utopian Tree Problem
    The Solution and Problem Statement for Utopian Tree Problem.

---
+++ Utopian Tree/UtopianTree/UtopianTree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtopianTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTests = Convert.ToInt32(Console.ReadLine());
+            List<int> heightsToWrite = new List<int>();
+            for (int i = 0; i < numberOfTests; i++)
+            {
+                int numberOfCycles = Convert.ToInt32(Console.ReadLine());
+                 heightsToWrite.Add(GetHeightByCycles(numberOfCycles));
+            }
+
+            foreach (int height in heightsToWrite)
+            {
+                Console.WriteLine(height);
+            }
+        }
+
+        public static int GetHeightByCycles(int numberOfCycles)
+        {
+            if (numberOfCycles == 0 || numberOfCycles == 1)
+            {
+                return numberOfCycles + 1;
+            }            
+
+            int height = 3;
+            for (int x = 3; x <= numberOfCycles; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    height++;
+                }
+                else
+                {
+                    height = height * 2;
+                }
+            }
+
+            return height;
+        }
+    }
+}
---
+++ Utopian Tree/UtopianTree/UtopianTreeTests/UtopianTreeTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtopianTree;
+
+namespace UtopianTreeTests
+{
+    [TestClass]
+    public class UtopianTreeTests
+    {
+        [TestMethod]
+        public void TestCaseOne()
+        {
+            // Arrange
+            int expected = 1;
+
+            // Act
+            int actual = Program.GetHeightByCycles(0);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCaseTwo()
+        {
+            // Arrange
+            int expected = 2;
+
+            // Act
+            int actual = Program.GetHeightByCycles(1);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCaseThree()
+        {
+            // Arrange
+            int expected = 7;
+
+            // Act
+            int actual = Program.GetHeightByCycles(4);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
