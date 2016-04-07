229d8ae5720bd775042ae45065948b2c92b83cab
EthanR <rivettea@hotmail.com>
2016-04-07 12:13:24 -0400

    Solution and Problem Statement for Pangram Problem
    Solution and Problem Statement for Pangram Problem. Includes console
    application solution and unit test project.

---
+++ Pangram/Pangram/Pangram/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pangram
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            string potentialPangram = Console.ReadLine();
+            Console.WriteLine(GetIsPangram(potentialPangram));
+        }
+
+        public static string GetIsPangram(string potentialPangram)
+        {
+            IEnumerable<char> characters = potentialPangram.ToUpper().Replace(" ", null).Distinct();
+            int charactersSize = characters.Count();
+            return charactersSize  >= 26 ? "pangram" : "not pangram";
+        }
+    }
+}
---
+++ Pangram/Pangram/PangramTests/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pangram;
+
+namespace PangramTests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void TestPangramCaseOne()
+        {
+            // Arrange
+            string potentialPangram = "We promptly judged antique ivory buckles for the next prize";
+            string expected = "pangram";
+
+            // Act
+            string actual = Solution.GetIsPangram(potentialPangram);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestNotPangramCaseOne()
+        {
+            // Arrange
+            string potentialPangram = "We promptly judged antique ivory buckles for the prize";
+            string expected = "not pangram";
+
+            // Act
+            string actual = Solution.GetIsPangram(potentialPangram);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
